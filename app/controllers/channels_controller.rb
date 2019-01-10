3d8b355ccae8dc4ab3bd7f3b35859e35a50ae345
菊地 聡美 <sato-chan.812@i.softbank.jp>
2019-01-10 09:52:32 +0900

    1/10 セレクトボックス途中

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -5,10 +5,21 @@ class ChannelsController < ApplicationController
   # include Youtube
 
     def index
-      @movieId = Youtube.search_videoid
+      # @movieId = Youtube.search_videoid
+    end
+
+
+    def create
+      if @category == 'JPOP'
+        @movieId = Youtube.search_videoid
+        redirect_to("/channnels/index")
+      end
     end
 
     def show
+
+      @movieId = Youtube.search_videoid
+
       # url = "https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCiX-HGeGIXJLIexjt1jEjWg&order=viewCount&type=playlist&maxResults=15&key=#{DEVELOPER_KEY}"
       #
       # result = Net::HTTP.get(URI.parse(url))
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'channels/index', to: 'channels#index'
   get 'channels/show', to: 'channels#show'
+  post 'channels/creat', to: 'channels#creat'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
